1ae819952691c02dc59325780c3dfc1dfaa81099
Jesse Zuniga <jzuniga206@gmail.com>
2020-05-24 20:35:07 -0700

    fixed secondary buttonc olor

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Link from 'next/link';
 
 import styles from './styles/Navbar.module.scss';
+// import logo from '../assets/templogo.png';
 
 const Navbar = () => {
   return (
     <nav id={styles.navbar}>
       <span className='logo'>
-        <img src='/assets/templogo.png' alt='logo' />
+        logo here... one day
+        {/* <img src={logo} alt='logo' /> */}
       </span>
       <div className={styles.navlinks}>
         <Link href='/'>Home</Link> / <Link href='/dashboard'>Dashboard</Link>
